bcceaa43688bcef33196294fa767a0a36180f871
hugh <18898553491@163.com>
2023-08-07 00:19:46 +0800

    新增功能及实现：实现统计指定专业下有多少报名参加的学生

--- school/src/com/imooc/model/Subject.java
+++ school/src/com/imooc/model/Subject.java
@@ -5,21 +5,31 @@ package com.imooc.model;
  * @author hugh
  */
 public class Subject {
-    // 成员属性：学科名称、学科编号、学制年限
+    // 成员属性：学科名称、学科编号、学制年限、报名选修的学生信息、报名选修的学生个数
     private String subjectName;
     private String subjectNo;
     private int subjectLife;
+    private Student[] myStudents;
+    private int studentNum;
 
     // 无参构造方法
     public Subject() {
 
     }
 
-    // 带参构造，实现对属性的全部赋值
+    // 带参构造，实现对学科名称、编号、学制年限赋值
     public Subject(String subjectName, String subjectNo, int subjectLife) {
         this.setSubjectName(subjectName);
         this.setSubjectNo(subjectNo);
         this.setSubjectLife(subjectLife);
+    }
+
+    // 带参构造，实现对全部属性的赋值
+    public Subject(String subjectName, String subjectNo, int subjectLife,Student[] myStudents) {
+        this.setSubjectName(subjectName);
+        this.setSubjectNo(subjectNo);
+        this.setSubjectLife(subjectLife);
+        this.setMyStudents(myStudents);
     }
 
     public String getSubjectName() {
@@ -48,6 +58,28 @@ public class Subject {
     }
 
     /**
+     * 获取选修专业的学生信息 如果保存学生信息的数组未被初始化，则 先初始化长度200
+     * @return 保存学生信息的数组
+     */
+    public Student[] getMyStudents() {
+        if(this.myStudents == null)
+            this.myStudents = new Student[200];
+        return myStudents;
+    }
+
+    public void setMyStudents(Student[] myStudents) {
+        this.myStudents = myStudents;
+    }
+
+    public int getStudentNum() {
+        return studentNum;
+    }
+
+    public void setStudentNum(int studentNum) {
+        this.studentNum = studentNum;
+    }
+
+    /**
      * 专业介绍的方法
      * @return 专业介绍的相关信息，包括名称、编号、年限
      */
@@ -56,4 +88,20 @@ public class Subject {
         return str;
     }
 
+    public void addStudent(Student stu) {
+        /**
+         * 1、将学生保存到数组中
+         * 2、将学生个数保存到 studentNum
+         */
+        // 1、将学生保存到数组中
+        for(int i=0; i < this.getMyStudents().length; i++) {
+            if(this.getMyStudents()[i] == null) {
+                stu.setStudentSubject(this);
+                this.getMyStudents()[i] = stu;
+                // 2、将学生个数保存到 studentNum
+                this.studentNum = i + 1;
+                return;
+            }
+        }
+    }
 }
